dc90f9dd6a6dc846c4f2fb34c62ffce8a154b504
Jason Go <jasongo@jasongo.net>
2023-03-07 04:08:50 +0800

    fix export settings

---
+++ lib/types/bun.d.ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono';
+import type { HonoRuntimeOptions, HonoRuntimeServe } from './common';
+declare class HonoRuntime extends Hono {
+    serve(options?: HonoRuntimeOptions): HonoRuntimeServe;
+}
+export { HonoRuntime as Hono };
+export * from 'hono';
+export * from './common';
+export * from 'hono/bun';
